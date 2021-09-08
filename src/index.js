77e32955397163a0b34ac28704133e69bec719ee
fabienramana <fabien.rmnd@gmail.com>
2021-09-08 22:04:59 +0200

    add config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const config = require('config');
 const bodyParser = require('body-parser')
 const express = require('express');
+const cors = require('cors')
+
 const apiRouter = require('./services/api');
-var cors = require('cors')
+const notFound = require('./middleware/notFound')
+const error = require('./middleware/error')
 
 const server = express();
 
@@ -12,6 +15,10 @@ server.use(cors())
 server.use(bodyParser.json())
 
 server.use(apiRouter);
+/*
+server.use(notFound);
+server.use(error);
+*/
 
 server.listen(config.get('port'), () => {
   console.log(`Example app listening on port ${config.get('port')}!`)
