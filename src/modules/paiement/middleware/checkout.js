820f92459b5cd21cd28f2456972e6353873c913c
Adrien <kaguraproject.sa@gmail.com>
2021-09-21 13:56:53 +0200

    modif checkout

--- src/modules/paiement/middleware/checkout.js
+++ src/modules/paiement/middleware/checkout.js
@@ -1,6 +1,8 @@
 const checkout = require('../service/checkout');
 const produits = require('../../produit/service/findProduitsByIds');
 const findProduitsByIds = require('../../produit/service/findProduitsByIds');
+const bcrypt = require('bcrypt')
+const createAchat = require('../../achat/service/createOne');
 
 module.exports = async (req, res, next) => {
 
@@ -8,6 +10,15 @@ module.exports = async (req, res, next) => {
     console.log(ids);
     const itemsToGet = await findProduitsByIds(ids);  // get items by ids [0,2,3,.....]
     console.log(itemsToGet);
+    date = '2021/09/21'
+    const achat = {
+        date: '2021/09/21',
+        utilisateur_id: 1,
+        achat_produit_id: 1,
+        transaction_id:  bcrypt.hashSync(Date.now().toString(), 10)
+    }
+
+    console.log(achat);
     const items = [];
     itemsToGet.forEach(obj => {
         let item = 
@@ -27,7 +38,7 @@ module.exports = async (req, res, next) => {
 
     console.log(JSON.stringify(items));
     try {
-        const url = await checkout(items);
+        const url = await checkout(items, achat.transaction_id);
         res.status(201).json( url ) 
     } catch(err){
         next(err);
