5b125baa358df9450e22d489e2b71230fc5d39df
fabienramana <fabien.rmnd@gmail.com>
2021-09-25 15:30:46 +0200

    update prix when offer is validated

--- src/modules/uses_cases/service/updateOfferStatus.js
+++ src/modules/uses_cases/service/updateOfferStatus.js
@@ -1,15 +1,17 @@
 const {findVenteById, updateVente} = require('../../vente/repository')
 const {findOffreById, updateOffre} = require('../../offre/repository')
 const {findProduitById, updateProduit} = require('../../produit/repository')
+const {findPrixVenteByTitre} = require('../../prix_vente/repository')
 
 module.exports = async (id, statut) =>{
     const offre = await findOffreById(id)
     const vente = await findVenteById(offre.venteId)
     const produit = await findProduitById(vente.produitId)
+    const prix_vente = await findPrixVenteByTitre(produit.titre)
 
     if (statut == "Accepté"){
         await updateOffre({ statut: "Accepté"}, id)
-        await updateProduit({statut: "En vente"}, produit.id)
+        await updateProduit({statut: "En vente", prix: prix_vente.prix + prix_vente.prix*0.3}, produit.id)
     }
     else if (statut == "Refusé"){
         await updateOffre({ statut: "Refusé"}, id)
