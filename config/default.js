a53790ec5eb18185c05b98f62053650eb0048176
fabienramana <fabien.rmnd@gmail.com>
2021-09-28 17:19:04 +0200

    change config port

--- config/default.js
+++ config/default.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: 8080,
+    port: process.env.PORT || 8080,
     one_signal_app_id: "c1502e68-2f67-4e54-8fcd-a0a44b22b291",
     one_signal_api_key: 'MTdhNzk3ZDctNjM0OC00ZDAzLTliNzMtZmMxYWI0ZDExZjFj'
   };
