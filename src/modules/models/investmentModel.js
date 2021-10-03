70da052fcbaaecda5fdb8aab5f2619b5fa6e3fd9
Adrien <kaguraproject.sa@gmail.com>
2021-10-03 13:56:33 +0200

    investment.amount -> investment.montant

--- src/modules/models/investmentModel.js
+++ src/modules/models/investmentModel.js
@@ -1,13 +1,17 @@
 const joi = require('@hapi/joi');
 
 const createModel = joi.object({
+    id: joi.number(),
     montant: joi.number().required(),
     projetId: joi.number().required(),
+    greenCoinId: joi.number()
 });
 
 const updateModel = joi.object({
+    id: joi.number(),
     montant: joi.number(),
     projetId: joi.number(),
+    greenCoinId: joi.number()
 });
 
 
